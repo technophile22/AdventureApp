bdeca2c81720f8fcd96bcf9e64b0a45e0dc3ecf6
technophile22 <raghavmangal22@gmail.com>
2020-11-11 22:37:49 +0530

    Tour description page

---
+++ app/src/main/java/com/example/adventureapp/description_page.java
@@ -0,0 +1,14 @@
+package com.example.adventureapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class description_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description_page);
+    }
+}
